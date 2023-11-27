58d916b7b4bc459fc80bf8f48a0ae0db5f09b19a
Lena2407 <lena_brueggemann@freenet.de>
2023-11-27 16:22:45 +0100

    Whoosh library implemented

--- crawler.py
+++ crawler.py
@@ -1,30 +1,45 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import re
 
+from whoosh.index import create_in
+from whoosh.index import open_dir
+from whoosh.fields import *
+from whoosh.query import *
+from whoosh.qparser import QueryParser
+
+
 TESTURL = 'https://vm009.rz.uos.de/crawl/index.html'
 UNIURL = 'https://www.uos.de'
 
-# uikit
+# uikit example for frontend
 
-# week 2: flask app and index with whoosh library
+# week 2: flask app 
 # flask app: 2 urls, 
 #   get home url (show search form)
 #   get search url with parameter q: 
 #       Search for q using the index and display a 
 #       list of URLs as links
 
-global index
+global ix
 
 def crawl(start_url):
     '''
     Crawls (gets and parses) all the HTML pages on a certain server
     '''
-    global index
+    global ix
+
+    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    if not os.path.exists("index"):
+        os.mkdir("index")
+    ix = create_in("index", schema)
+
+    writer = ix.writer()
+
 
     urls = [start_url]
     visited_urls = []
-    index = {}
 
     split_url = start_url.split('/')
     server = split_url[2]
@@ -35,14 +50,18 @@ def crawl(start_url):
         visited_urls.append(current_url)
 
         # crawl for new url links
-        response = requests.get(current_url, timeout=0.1) # get requests
+        response = requests.get(current_url, timeout=5) # get requests
         status = response.status_code
         if status != 200: # status code 200 means everything went well
             continue
         soup = BeautifulSoup(response._content, 'html.parser') #print(soup.title.text) #print(soup.text) # text of entire page without html tags
-       
-        update_index(index, current_url, soup.text)
+        current_title = soup.title.text
 
+        print ('Title: ', current_title)
+        print ('Path: ', current_url)
+        print ('Content: ', soup.text)
+        
+        writer.add_document(title=current_title, path=current_url, content=soup.text)
         
         # find links (urls) in content
         # <a href="...">Text</a>
@@ -72,72 +91,38 @@ def crawl(start_url):
 
             else:
                 print('did not understand url: ', url)
+
     print(visited_urls)
-
-    index = dict( sorted(index.items(), key=lambda x: x[0].lower()) )
-
-
-    for k,v in index.items():
-        print('{}:{}'.format(k,v))
+    writer.commit()
 
 
-    #return urls
-    
-def update_index(index, url, text):
-    print('index_update')
-    words = list(set(re.sub(r'[^\w\s]','',text).split())) # remove doubled entries
-    #print(words)
+def search(querystring):
+    print('starts searching')
 
-    for word in words:
-        if word in index.keys():
-            index[word].append(url)
-        else:
-            index[word] = [url]
+    global ix
 
-    index = dict( sorted(index.items(), key=lambda x: x[0].lower()) ) # sort list
+    with ix.searcher() as searcher:
+        parser = QueryParser("content", ix.schema)
+        myquery = parser.parse(querystring)
+        results = searcher.search(myquery)
 
-
+        print('Number Results: ', len(results))
+        if len(results) > 0:
+            for result in results:
+                print(result)
 
 
-def search(keys):
-    print('SEARCH')
-    global index
-    urls = [] # urls that contain all keywords
-
-    possible_urls = [] # candidates
-
-    for key in keys:
-        possible_urls.extend(index[key])
-    possible_urls = list(set(possible_urls))
-    urls = possible_urls
-
-    print('Possible URLS: ', possible_urls)
-
-    removing_urls = []
-    for key in keys:
-        print('Key: ', key)
-        print('Right URLS: ', index[key])
-        for url in possible_urls:
-            print('checking url: ', url,' for key ', key)
-            if url not in index[key]:
-                print('not in key ', key, ' is: ', url)
-                removing_urls.append(url)
-    for url in removing_urls:
-        urls.remove(url)
-    
-
-    return urls
 
 
 def main():
+    global ix
     
     crawl(TESTURL)
-
-    keywords = ['that', 'That'] # casefold? ignore upper or lower case letters!!
-    results = search(keywords)
-    print('RESULTS: ', results)
+    #crawl(UNIURL)
 
 
+    querystring_example = "Page"
+    search(querystring_example)
 
 
 if __name__ == "__main__":
